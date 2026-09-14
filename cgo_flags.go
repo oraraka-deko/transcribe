9bec50c042c01351db6dd47abba85a004ffe39ec
Your Name <you@example.com>
2026-09-13 20:38:33 -0700

    initial commit

---
+++ cgo_flags.go
@@ -0,0 +1,12 @@
+package main
+
+/*
+#cgo CFLAGS: -I${SRCDIR}/whisper.cpp/include -I${SRCDIR}/whisper.cpp/ggml/include
+
+// --- Linux Configuration ---
+#cgo linux LDFLAGS: -L${SRCDIR}/whisper.cpp/buildlin/src -L${SRCDIR}/whisper.cpp/buildlin/ggml/src -lwhisper -lggml -lstdc++ -lm
+
+// --- Windows Configuration ---
+#cgo windows LDFLAGS: -L${SRCDIR}/whisper.cpp/buildwin/src -L${SRCDIR}/whisper.cpp/buildwin/ggml/src -lwhisper -lggml -lstdc++ -lm -lpathcch
+*/
+import "C"
---
+++ go.mod
@@ -0,0 +1,7 @@
+module transcribe
+
+go 1.27.1
+
+require github.com/ggerganov/whisper.cpp/bindings/go v0.0.0-20260911141324-1da4dc82fa79
+
+replace github.com/ggerganov/whisper.cpp/bindings/go => ./whisper.cpp/bindings/go
